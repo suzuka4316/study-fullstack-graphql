9ced99fd358c254fab21c93b45b5a96555de9aa9
SuzukaF <suzuka.fukami@gmail.com>
2023-03-05 14:02:17 +1100

    resolver to return array of pets

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,18 +1,23 @@
 module.exports = {
+  // Step 3: define resolvers
   Query: {
-
-  },
-  Mutation: {
-
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    // top level resolver because it belongs to type Query
+    pets(_, __, context) { // initial value, arguments, context passed in server
+      return context.models.Pet.findMany()
     }
   },
-  User: {
+  // Mutation: {
 
-  }
+  // },
+  //// each type can have it's own default resolver. it runs after the top resolvers run
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+
+  // }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -1,6 +1,23 @@
 const { gql } = require('apollo-server')
 
+// Step 1: Create type definitions based on db schema
 const typeDefs = gql`
+  type User {
+    id: ID! # unique identifier
+    username: String!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  # Step 2: Create Query type
+  type Query {
+    pets: [Pet]!
+  }
 `
 
 module.exports = typeDefs
