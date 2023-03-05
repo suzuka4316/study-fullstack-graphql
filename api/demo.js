5ffcfdcc79f65a8c9dbae2391742d910f9a27da4
SuzukaF <suzuka.fukami@gmail.com>
2023-03-05 12:54:11 +1100

    memory db setup and template

--- api/demo.js
+++ api/demo.js
@@ -1,3 +1,11 @@
+/**
+ * GraphQL server has only one endpoint.
+ * It does not respect HTTP 
+ * It always returns 200 even if errors occured
+ * Only thing that's dynamic is the Query client sends
+ * Query you send dictates what resolvers get executed
+ */
+
 const gql = require("graphql-tag")
 const { ApolloServer } = require("apollo-server")
 
@@ -12,10 +20,11 @@ const typeDefs = gql`
   # Query Definition - description of how a client can access this type
   type Query {
     me: User! # non-null User
+    friends: [User]!
   }
 `
 
-// Resolver
+// Resolver - functions that return values for fields that's defined in Types
 const resolvers = {
   // has to be the same name as type definition
   Query: {
---
+++ api/src/schema.js
@@ -0,0 +1,6 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+`
+
+module.exports = typeDefs
